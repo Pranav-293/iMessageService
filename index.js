4b0aae2254a2a6bde0bad9165d7d8d32a8f7eb06
Pranav Gupta <Prangupta@paymentus.com>
2023-08-07 11:19:28 +0530

    some changes

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.post("/", (req, res) => {
       const recipient = req.body.recipient;
       const message = `Received you message - ${req.body.text}`;
        Imessage.postMessage(sender_name, recipient, message);
+       console.log(req.body);
        res.send();
     }
     else{
